688da12e8ab14de019f83748b09826b1b4071490
Christopher Coco <chriscoco1205@gmail.com>
2025-08-02 13:38:03 -0400

    make background transparent

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -3,7 +3,9 @@ return {
     "rebelot/kanagawa.nvim",
     name = "kanagawa",
     config = function()
-      require("kanagawa").setup({})
+      require("kanagawa").setup({
+        transparent = true,
+      })
     end,
   },
   {
